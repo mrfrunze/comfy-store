bd99faf6e644071459e40ccab7915f8dba2c6eca
Ilya <stoynov.oleg@gmail.com>
2024-03-27 19:12:27 +0200

    commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {About, Cart, Checkout, Error} from "./pages/index"
+const App = () => {
+  return (
+    <>
+     <About/>
+     <Checkout/>
+     <Cart/> 
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export {default as About} from "./About"
+export {default as Cart} from "./Cart"
+export {default as Checkout} from "./Checkout"
+export {default as Error} from "./Error"
