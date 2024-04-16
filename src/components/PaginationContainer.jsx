e7e7e37cf63318c3cd2870090d55bef7d5ae595a
Ilya <stoynov.oleg@gmail.com>
2024-04-16 20:37:09 +0300

    commit

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const PaginationContainer = () => {
   return (
     <div>PaginationContainer</div>
--- src/components/index.js
+++ src/components/index.js
@@ -11,3 +11,4 @@ export {default as SectionTitle} from "./SectionTitle";
 export {default as Filters} from "./Filters";
 export {default as PaginationContainer} from "./PaginationContainer";
 export {default as ProductsContainer} from "./ProductsContainer";
+export {default as ProductsList} from "./ProductsList";
