ce0024faf6f1af1c02189a1493b785f802d2c93b
Ilya <stoynov.oleg@gmail.com>
2024-04-18 16:55:57 +0300

    commit

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,7 +1,8 @@
 import { Form, useLoaderData, Link } from 'react-router-dom';
-import FormInput from './FormInput';
+import {FormInput, FormSelect, FormRange, FormCheckbox} from './index';
 
 const Filters = () => {
+  const {meta} = useLoaderData()
   return (
     <Form className='bg-base-200 rounded-md py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center'>
       {/* search */}
@@ -11,6 +12,16 @@ const Filters = () => {
         name="search" 
         size="input-sm" 
       />
+      {/* category */}
+      <FormSelect label="select category" name="category" list={meta.categories} size="select-sm" />
+      {/* companies */}
+      <FormSelect label="select company" name="company" list={meta.companies} size="select-sm" />
+      {/* order */}
+      <FormSelect label="sort by" name="order" list={['a-z', 'z-a', 'hight', 'low']} size="select-sm" />
+      {/* Range price */}
+      <FormRange name="price" label="select price" size="select-sm" />
+      {/* SHIPPING */}
+      <FormCheckbox name="shipping" label="free shipping" size="checkbox-sm" />
       {/* buttons */}
       <button type="submit" className='btn btn-primary btn-sm'>
         search
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,17 +1,24 @@
-import { Outlet } from "react-router-dom"
-import { Header, Navbar } from "../components"
+import { Outlet, useNavigation } from "react-router-dom"
+import { Header, Navbar, Loading } from "../components"
 
 
 
 const HomeLayout = () => {
+  const navigation = useNavigation()
+  const isPageLoading = navigation.state === "loading"
   return (
     <>
       
       <Header/>
       <Navbar/>
-      <section className="aline-element py-20">
-         <Outlet />
-      </section>
+      {isPageLoading ? (
+        <Loading/> 
+      ) : (
+        <section className="aline-element py-20">
+          <Outlet />
+        </section> 
+      )}
+     
      
     </>
   )
