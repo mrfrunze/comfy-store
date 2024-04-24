b9d56cdcc7627c8938bb9bc5876259bff35129af
Ilya <stoynov.oleg@gmail.com>
2024-04-24 19:30:41 +0300

    commit

--- src/components/PaginationContainer.jsx
+++ src/components/PaginationContainer.jsx
@@ -1,6 +1,61 @@
+import { useLoaderData, useLocation, useNavigate } from "react-router-dom"
+
 const PaginationContainer = () => {
+  const {meta} = useLoaderData()
+  // console.log(meta);
+  const { page, pageCount, pageSize, total } = meta.pagination
+
+  const pages = Array.from({length:pageCount}, (_, index) => {
+    return index + 1
+  })
+
+  const {search, pathname} = useLocation()
+  const navigate = useNavigate()
+
+  const handlePageChange = pageNumber => {
+    // console.log(search);
+    const searchParams = new URLSearchParams(search)
+    searchParams.set("page", pageNumber)
+    // console.log(pathname);
+    navigate(`${pathname}?${searchParams.toString()}`)
+  }
+
+  if(pageCount < 2) return null
+
   return (
-    <div>PaginationContainer</div>
+    <div className="mt-16 flex justify-end">
+      <div className="join">
+        <button 
+          className="btn btn-xs sm:btn-md join-item" 
+          onClick={() => {
+            let prevPage = page - 1
+            if (prevPage < 1) prevPage = pageCount
+            handlePageChange(prevPage)
+          }}>
+          Prev
+        </button>
+        {pages.map(pageNumber => {
+          return (
+            <button 
+              key={pageNumber} 
+              className={`btn btn-xs sm:btn-md join-item ${
+                pageNumber === page ? "bg-base-300 border-base-300" : " "}`} 
+              onClick={() => handlePageChange(pageNumber)}>
+              {pageNumber}
+            </button>
+          )
+        })}
+        <button 
+          className="btn btn-xs sm:btn-md join-item" 
+          onClick={() => {
+            let nextPage = page + 1;
+            if(nextPage > pageCount) nextPage = 1
+            handlePageChange(nextPage)
+          } }>
+          Next
+        </button>
+      </div>
+    </div>
   )
 }
 
