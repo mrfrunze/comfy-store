1588b51e34902d9f8f007e56340820ff4288cfb3
Ilya <stoynov.oleg@gmail.com>
2024-05-06 18:25:10 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { ErrorElement } from "./components"
 import {loader as landingLoader} from "./pages/Landing"
 import {loader as singleProductLoader} from "./pages/SingleProduct"
 import {loader as productsLoader} from "./pages/Products"
+import { loader as checkoutLoader } from './pages/Checkout';
 
 // action
 import {action as registerAction} from "./pages/Register"
@@ -47,7 +48,8 @@ const router = createBrowserRouter([
       },
       {
         path: "/checkout",
-        element: <Checkout/>
+        element: <Checkout/>,
+        loader: checkoutLoader(store),
       },
       {
         path: "/orders",
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -7,6 +7,10 @@ const themes = {
     night: "night",
 }
 
+const getUserFromLocalStorage = () => {
+   return JSON.parse(localStorage.getItem("user")) || null;
+}
+
 
 const getThemeLocalStorage = () => {
     const theme = localStorage.getItem("theme") || themes.winter;
@@ -15,7 +19,7 @@ const getThemeLocalStorage = () => {
 }
 
 const initialState = {
-    user: { username: "codding addict" },
+    user: getUserFromLocalStorage(),
     theme: getThemeLocalStorage()
 }
 
@@ -24,7 +28,10 @@ const userSlice = createSlice({
     initialState,
     reducers: {
         loginUser: (state, action) => {
-            console.log("login");
+            // console.log(action.payload);
+            const user = {...action.payload.user, token: action.payload.jwt}
+            state.user = user
+            localStorage.setItem("user", JSON.stringify(user))
         },
         logoutUser: (state) => {
             state.user = null
